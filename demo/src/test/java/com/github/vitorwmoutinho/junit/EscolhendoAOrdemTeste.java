11e0bd45d2c977a58b7f66d4a742651f6fe41f73
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-06 16:17:12 -0300

    Aprendendo sobre display

--- demo/src/test/java/com/github/vitorwmoutinho/junit/EscolhendoAOrdemTeste.java
+++ demo/src/test/java/com/github/vitorwmoutinho/junit/EscolhendoAOrdemTeste.java
@@ -28,25 +28,26 @@ import org.junit.jupiter.api.*;
 
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 public class EscolhendoAOrdemTeste {
-    @Order(4)
+
     @DisplayName("Teste que valida se o usuário foi criado")
     @Test
+    @DisplayName("A")
     void validaFluxoA() {
         Assertions.assertTrue(true);
     }
-    @Order(3)
+
     @DisplayName("B")
     @Test
     void validaFluxoB() {
         Assertions.assertTrue(true);
     }
-    @Order(2)
+
     @DisplayName("C")
     @Test
     void validaFluxoC() {
         Assertions.assertTrue(true);
     }
-    @Order(1)
+
     @DisplayName("D")
     @Test
     void validaFluxoD() {
